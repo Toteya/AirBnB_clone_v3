f1a540aba066de729f542d25227f6884fe090510
Toteya <111172685+Toteya@users.noreply.github.com>
2023-10-30 14:00:39 +0200

    web api

---
+++ api/v1/app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+A flask web application
+"""
+from api.v1.views import app_views
+from flask import Flask
+from models import storage
+import os
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def close_storage(exception=None):
+    """ calls method close """
+    storage.close()
+
+
+if __name__ == "__main__":
+    host = os.environ.get("HBNB_API_HOST")
+    if host is None:
+        host = "0.0.0.0"
+    port = os.environ.get("HBNB_API_PORT")
+    if port is None:
+        port = "5000"
+    app.run(host=host, port=port, threaded=True)
